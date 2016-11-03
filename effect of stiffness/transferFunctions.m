05c873d9439c65b0b46bf60bd71da1f6e5212377
fjam100 <jamesf@oregonstate.edu>
2016-11-02 17:59:48 -0700

    Initial upload

---
+++ effect of stiffness/transferFunctions.m
@@ -0,0 +1,29 @@
+%% Scripts to look at transfer functions
+% Author: Francis James
+% Model: [mass_m]-[spring and damper]-[load_l]
+
+%% Set up parameters
+model.k=100;    %Spring stiffness
+model.c=1;      %damping
+model.m_m=1;    %motor inertia
+model.m_l=1;    %load inertia
+
+controller.P=1;
+controller.D=0;
+
+
+%% open loop transfer functions
+%1) X_l/Xm
+olsys1=tf([model.c,model.k],[model.m_l, model.c, model.k]);
+%2) X_l/F_app
+olsys2=tf([model.c, model.k],[model.m_l*model.m_m, model.c*(model.m_m+model.m_l), ...
+    model.k*(model.m_m+model.m_l),0,0]);
+%3) X_m/F_app
+olsys3=tf([model.m_l, model.c, model.k],[model.m_l*model.m_m, model.c*(model.m_m+model.m_l), ...
+    model.k*(model.m_m+model.m_l),0,0]);
+
+%% Closed loop transfer functions
+% Using PD controller with (x_l-x_m)_desired and its derivative = 0
+%1) (x_l-x_m)/F_d
+clsys1=tf([1],model.m_l*[-1, (model.c/model.m_m+model.c/model.m_l-controller.D/model.m_m) ...
+    model.k/model.m_m+model.k/model.m_l-controller.P/model.m_m]);
