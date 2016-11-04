becb80a9c2b1b115a62502b44a42e932f9b59621
francis <francisjames10@gmail.com>
2016-11-04 14:23:01 -0700

    Updated/corrected transfer functions, built simulink model for system

---
+++ effect of stiffness/main_physicalModel.m
@@ -0,0 +1,8 @@
+%% Script for setting parameters for physical model.slx
+c=10;
+k=100;
+P=1000;
+D=100;
+
+m_m=1;
+m_l=1;
--- effect of stiffness/transferFunctions.m
+++ effect of stiffness/transferFunctions.m
@@ -2,14 +2,16 @@
 % Author: Francis James
 % Model: [mass_m]-[spring and damper]-[load_l]
 
+clear all;
+
 %% Set up parameters
 model.k=100;    %Spring stiffness
 model.c=1;      %damping
 model.m_m=1;    %motor inertia
 model.m_l=1;    %load inertia
 
-controller.P=1;
-controller.D=0;
+controller.P=-1000;
+controller.D=-100;
 
 
 %% open loop transfer functions
@@ -25,5 +27,13 @@ olsys3=tf([model.m_l, model.c, model.k],[model.m_l*model.m_m, model.c*(model.m_m
 %% Closed loop transfer functions
 % Using PD controller with (x_l-x_m)_desired and its derivative = 0
 %1) (x_l-x_m)/F_d
-clsys1=tf([1],model.m_l*[-1, (model.c/model.m_m+model.c/model.m_l-controller.D/model.m_m) ...
+clsys1=tf([1/model.m_l],[1, (model.c/model.m_m+model.c/model.m_l-controller.D/model.m_m) ...
     model.k/model.m_m+model.k/model.m_l-controller.P/model.m_m]);
+%2) Xl/Fd
+clsys2=tf([model.m_m, model.c-controller.D, model.k-controller.P],[model.m_m*model.m_l, ... 
+    model.c*model.m_l+model.c*model.m_m-controller.D*model.m_l, model.k*model.m_l+model.k*model.m_m ...
+    -controller.P*model.m_l]);
+%3) Xm/Fd
+clsys3=tf([model.c-controller.D, -controller.P+model.k],[model.m_m*model.m_l, ... 
+    model.c*model.m_l+model.c*model.m_m-controller.D*model.m_l, model.k*model.m_l+model.k*model.m_m ...
+    -controller.P*model.m_l]);
