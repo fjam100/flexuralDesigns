cac3bec413b4d8378c4c85ee3e156ac154dc99a9
francis <francisjames10@gmail.com>
2016-11-04 17:18:57 -0700

    Effect of proportional gain added

--- effect of stiffness/main_plotsAndFigures.m
+++ effect of stiffness/main_plotsAndFigures.m
@@ -16,6 +16,24 @@ for i=1:length(k)
 end
 
 
-% To show effect of chanign k, first show the pole zero map to show how the
+% To show effect of changing k, first show the pole zero map to show how the
 % settling time changes, but show how faster settling time with higher
 % stiffness is offset by higher gains in bode plot
+
+%% Changing proportional gain
+clear all;
+k=100;
+m_l=1;
+m_m=1;
+c=2*sqrt(k*(1/m_m+1/m_l))*(m_m*m_l)/(m_m+m_l);
+P=(-1000:-10:-1000);
+sys=cell(length(P),1);
+for i=1:length(P)
+    D(i)=-m_m*(2*sqrt((k/m_m+k/m_l-P(i)/m_m))-c/m_m-c/m_l);
+    tfn=tf([k/m_l],[1, c/m_m+c/m_l-D(i)/m_m, k/m_m+k/m_l-P(i)/m_m]);
+    sys{i}=tfn;
+    pzmap(sys{i});
+    hold on;
+end
+
+%% Increasing controller stiffness results in reduction in both settling time and peak amplitude in the bode plot
